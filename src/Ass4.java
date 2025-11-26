b493ab962c3cb105916644d38af0eb2bbdbbc17d
OrSasson1407 <orsasson1407@gmail.com>
2025-11-26 22:00:04 +0200

    FirstCommit

--- src/Ass4.java
+++ src/Ass4.java
@@ -1,12 +1,10 @@
 class Ass4 {
     private int bar = 0;
 
-    // שינוי נדרש: הוספת synchronized למתודה baz()
     public synchronized void baz() {
         bar++;
     }
 
-    // שינוי נדרש: הוספת synchronized למתודה getBar()
     public synchronized int getBar() {
         return bar;
     }
@@ -14,6 +12,7 @@ class Ass4 {
     public static void main(String[] args) throws InterruptedException {
         Ass4 f = new Ass4();
         Thread[] arr = new Thread[2];
+
         for (int i = 0; i < arr.length; i++) {
             arr[i] = new Thread(() -> {
                 for (int j = 0; j < 10000; j++) {
@@ -22,9 +21,11 @@ class Ass4 {
             });
             arr[i].start();
         }
+
         for (int i = 0; i < arr.length; i++) {
             arr[i].join();
         }
+
         System.out.println(f.getBar());
     }
 }
