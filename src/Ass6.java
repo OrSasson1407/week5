7e4e7f7d9dff13dc0d775aeed76be6c601c2ba06
OrSasson1407 <orsasson1407@gmail.com>
2025-11-26 22:29:09 +0200

    FirstCommit

---
+++ src/Ass6.java
@@ -0,0 +1,22 @@
+class Ass6 {
+    private int bar = 0;
+    public void baz() { bar++; }
+    public int getBar() { return bar; }
+    public static void main(String[] args) throws InterruptedException {
+        final long start = System.currentTimeMillis();
+        Ass6 f = new Ass6();
+        Thread[] arr = new Thread[10];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = new Thread(() -> {
+                for (int j = 0; j < 10000000; j++) {
+                    f.baz();
+                }
+            });
+            arr[i].start();
+        }
+        for (int i = 0; i < arr.length; i++) {
+            arr[i].join();
+        }
+        System.out.println(f.getBar() + " " + (System.currentTimeMillis() - start));
+    }
+}
